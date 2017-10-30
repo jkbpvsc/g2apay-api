e087270220c17e1366ce287d2f7f6f3930a07cc3
Jakob Povšič <jakob.povsic@gmail.com>
2017-10-30 23:21:57 +0100

    Initial commit

---
+++ resources/ECartType.js
@@ -0,0 +1,4 @@
+module.exports = {
+    Physical: 'physical',
+    Digital: 'digital'
+};
---
+++ resources/EHostAddresses.js
@@ -0,0 +1,26 @@
+/**
+ * G2A host addresses
+ */
+module.exports =
+{
+    Quote: {
+        production: 'checkout.pay.g2a.com',
+        sandbox: 'checkout.test.pay.g2a.com'
+    },
+
+    Merchant: {
+        production: 'pay.g2a.com',
+        sandbox: 'www.test.pay.g2a.com'
+    },
+
+    Rest: {
+        production: 'pay.g2a.com',
+        sandbox: 'www.test.pay.g2a.com'
+    },
+
+    Types: {
+        Quote: 'Quote',
+        Merchant: 'Merchant',
+        Rest: 'Rest'
+    }
+};
