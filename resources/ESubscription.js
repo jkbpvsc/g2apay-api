82ce33f45575a4340f338cee99206ff8b3317afa
fuckoff <jkbpvsc@tuta.io>
2018-01-04 00:18:37 +0100

    Subscription support

---
+++ resources/ESubscription.js
@@ -0,0 +1,18 @@
+module.exports = {
+    IPN_TYPE: {
+        subscription_canceled: 'subscription_canceled',
+        subscription_created: 'subscription_created'
+    },
+    PERIODS: {
+        daily:          'daily',
+        weekly:         'weekly',
+        monthly:        'monthly',
+        quarterly:      'quarterly',
+        semi_annually : 'semi-annually',
+        annually :      'annually',
+    },
+    TYPE: {
+        product: 'product',
+        donation: 'donation'
+    }
+};
